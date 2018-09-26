f2d1e43fe5e055ac558b05a7686619e1e7d208fc
Mohamed Salah Zidane <mohamed.zidane95@gmail.com>
2018-09-26 15:45:17 +0200

    change full Project name

--- Restaurant DiscoveryPin/AppDelegate.swift
+++ Restaurant DiscoveryPin/AppDelegate.swift
@@ -54,7 +54,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "ZidaneFood")
+        let container = NSPersistentContainer(name: "RestaurantDiscoveryPin")
         container.loadPersistentStores(completionHandler: { (storeDescription,
             error) in
             if let error = error as NSError? {
